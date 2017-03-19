7e5f90bbcf15808a507a3256d610fe8e6e2e1b10
Peter Hänsgen <phaensgen@demandware.com>
2017-03-19 20:37:22 +0100

    NOR gate added

--- src-test/sunday/resi/library/NorTest.java
+++ src-test/sunday/resi/library/NorTest.java
@@ -12,155 +12,152 @@ import sunday.resi.common.Power;
 import sunday.resi.common.Signal;
 
 /**
- * Tests the AND gate.
+ * Tests the NOR gate.
  * 
  * @author Peter H&auml;nsgen
  */
-public class AndTest
+public class NorTest
 {
     /**
-     * Tests an AND with 1 input. The output value simply follows the input value.
+     * Tests a NOR with 1 input. The output value simply follows the input value.
      */
     @Test
-    public void testAnd1()
+    public void testNor1()
     {
         Circuit circuit = new Circuit();
         Power power = new Power(circuit, "VCC");
 
-        And and = new And(circuit, "And", 1);
+        Nor nor = new Nor(circuit, "Nor", 1);
 
         Output out0 = new Output();
         Input in = new Input();
 
-        new Signal(circuit).from(power.getOut()).to(and.getPowerIn());
-        new Signal(circuit).from(out0).to(and.getIn(0));
-        new Signal(circuit).from(and.getOut()).to(in);
+        new Signal(circuit).from(power.getOut()).to(nor.getPowerIn());
+        new Signal(circuit).from(out0).to(nor.getIn(0));
+        new Signal(circuit).from(nor.get_Out()).to(in);
 
         out0.setValue(null);
-        circuit.simulate();
-        assertNull(in.getValue());
-
-        out0.setValue(true);
         circuit.simulate();
         assertTrue(in.getValue());
 
-        out0.setValue(null);
+        out0.setValue(true);
         circuit.simulate();
         assertNull(in.getValue());
+
+        out0.setValue(null);
+        circuit.simulate();
+        assertTrue(in.getValue());
     }
 
     /**
-     * Tests an AND with 2 inputs.
+     * Tests a NOR with 2 inputs.
      */
     @Test
-    public void testAnd2()
+    public void testNor2()
     {
         Circuit circuit = new Circuit();
         Power power = new Power(circuit, "VCC");
 
-        And and = new And(circuit, "And", 2);
+        Nor nor = new Nor(circuit, "Nor", 2);
 
         Output out0 = new Output();
         Output out1 = new Output();
         Input in = new Input();
 
-        new Signal(circuit).from(power.getOut()).to(and.getPowerIn());
-        new Signal(circuit).from(out0).to(and.getIn(0));
-        new Signal(circuit).from(out1).to(and.getIn(1));
-        new Signal(circuit).from(and.getOut()).to(in);
+        new Signal(circuit).from(power.getOut()).to(nor.getPowerIn());
+        new Signal(circuit).from(out0).to(nor.getIn(0));
+        new Signal(circuit).from(out1).to(nor.getIn(1));
+        new Signal(circuit).from(nor.get_Out()).to(in);
 
         out0.setValue(null);
         out1.setValue(null);
-        circuit.simulate();
-        assertNull(in.getValue());
-
-        out0.setValue(true);
-        out1.setValue(null);
-        circuit.simulate();
-        assertNull(in.getValue());
-
-        out0.setValue(null);
-        out1.setValue(true);
-        circuit.simulate();
-        assertNull(in.getValue());
-
-        out0.setValue(true);
-        out1.setValue(true);
         circuit.simulate();
         assertTrue(in.getValue());
+
+        out0.setValue(true);
+        out1.setValue(null);
+        circuit.simulate();
+        assertNull(in.getValue());
+
+        out0.setValue(null);
+        out1.setValue(true);
+        circuit.simulate();
+        assertNull(in.getValue());
+
+        out0.setValue(true);
+        out1.setValue(true);
+        circuit.simulate();
+        assertNull(in.getValue());
     }
 
     /**
-     * Tests an AND with 3 inputs.
+     * Tests a NOR with 3 inputs.
      */
     @Test
-    public void testAnd3()
+    public void testNor3()
     {
         Circuit circuit = new Circuit();
         Power power = new Power(circuit, "VCC");
 
-        And and = new And(circuit, "And", 3);
+        Nor nor = new Nor(circuit, "Nor", 3);
 
         Output out0 = new Output();
         Output out1 = new Output();
         Output out2 = new Output();
         Input in = new Input();
 
-        new Signal(circuit).from(power.getOut()).to(and.getPowerIn());
-        new Signal(circuit).from(out0).to(and.getIn(0));
-        new Signal(circuit).from(out1).to(and.getIn(1));
-        new Signal(circuit).from(out2).to(and.getIn(2));
-        new Signal(circuit).from(and.getOut()).to(in);
-
-        circuit.simulate();
-        assertNull(in.getValue());
+        new Signal(circuit).from(power.getOut()).to(nor.getPowerIn());
+        new Signal(circuit).from(out0).to(nor.getIn(0));
+        new Signal(circuit).from(out1).to(nor.getIn(1));
+        new Signal(circuit).from(out2).to(nor.getIn(2));
+        new Signal(circuit).from(nor.get_Out()).to(in);
 
         out0.setValue(null);
         out1.setValue(null);
         out2.setValue(null);
-        circuit.simulate();
-        assertNull(in.getValue());
-
-        out0.setValue(true);
-        out1.setValue(null);
-        out2.setValue(null);
-        circuit.simulate();
-        assertNull(in.getValue());
-
-        out0.setValue(null);
-        out1.setValue(true);
-        out2.setValue(null);
-        circuit.simulate();
-        assertNull(in.getValue());
-
-        out0.setValue(true);
-        out1.setValue(true);
-        out2.setValue(null);
-        circuit.simulate();
-        assertNull(in.getValue());
-
-        out0.setValue(null);
-        out1.setValue(null);
-        out2.setValue(true);
-        circuit.simulate();
-        assertNull(in.getValue());
-
-        out0.setValue(true);
-        out1.setValue(null);
-        out2.setValue(true);
-        circuit.simulate();
-        assertNull(in.getValue());
-
-        out0.setValue(null);
-        out1.setValue(true);
-        out2.setValue(true);
-        circuit.simulate();
-        assertNull(in.getValue());
-
-        out0.setValue(true);
-        out1.setValue(true);
-        out2.setValue(true);
         circuit.simulate();
         assertTrue(in.getValue());
+
+        out0.setValue(true);
+        out1.setValue(null);
+        out2.setValue(null);
+        circuit.simulate();
+        assertNull(in.getValue());
+
+        out0.setValue(null);
+        out1.setValue(true);
+        out2.setValue(null);
+        circuit.simulate();
+        assertNull(in.getValue());
+
+        out0.setValue(true);
+        out1.setValue(true);
+        out2.setValue(null);
+        circuit.simulate();
+        assertNull(in.getValue());
+
+        out0.setValue(null);
+        out1.setValue(null);
+        out2.setValue(true);
+        circuit.simulate();
+        assertNull(in.getValue());
+
+        out0.setValue(true);
+        out1.setValue(null);
+        out2.setValue(true);
+        circuit.simulate();
+        assertNull(in.getValue());
+
+        out0.setValue(null);
+        out1.setValue(true);
+        out2.setValue(true);
+        circuit.simulate();
+        assertNull(in.getValue());
+
+        out0.setValue(true);
+        out1.setValue(true);
+        out2.setValue(true);
+        circuit.simulate();
+        assertNull(in.getValue());
     }
 }
---
+++ src/sunday/resi/library/Nor.java
@@ -0,0 +1,101 @@
+package sunday.resi.library;
+
+import sunday.resi.common.Circuit;
+import sunday.resi.common.Component;
+import sunday.resi.common.Input;
+import sunday.resi.common.Output;
+import sunday.resi.common.Signal;
+
+/**
+ * A logical NOR gate built with relays.
+ * 
+ * @author Peter H&auml;nsgen
+ */
+public class Nor extends Component
+{
+    private Input powerIn;
+
+    private Input[] ins;
+
+    private Output _out;
+
+    /**
+     * The constructor.
+     * 
+     * @param n number of inputs
+     */
+    public Nor(Circuit circuit, String name, int n)
+    {
+        super(circuit, name);
+
+        powerIn = new Input();
+
+        ins = new Input[n];
+
+        Relay[] relays = new Relay[n];
+        for (int i = 0; i < n; i++)
+        {
+            ins[i] = new Input();
+
+            Relay r = new Relay(circuit, name + "_R" + String.valueOf(i));
+            relays[i] = r;
+
+            // internal wiring
+            new Signal(circuit).from(ins[i]).to(r.getCoilIn());
+
+            if (i == 0)
+            {
+                new Signal(circuit).from(powerIn).to(r.getMiddleIn(0));
+            }
+            else
+            {
+                new Signal(circuit).from(relays[i - 1].get_Out(0)).to(r.getMiddleIn(0));
+            }
+        }
+
+        _out = new Output();
+
+        new Signal(circuit).from(relays[n - 1].get_Out(0)).to(_out);
+    }
+
+    public Input getPowerIn()
+    {
+        return powerIn;
+    }
+
+    public Input getIn(int index)
+    {
+        return ins[index];
+    }
+
+    public Output get_Out()
+    {
+        return _out;
+    }
+
+    @Override
+    public String toString()
+    {
+        StringBuilder sb = new StringBuilder();
+        sb.append("[NOR ");
+        sb.append(getName());
+        sb.append(": ");
+
+        for (int i = 0; i < ins.length; i++)
+        {
+            Input in = ins[i];
+
+            sb.append("in");
+            sb.append(String.valueOf(i));
+            sb.append("=");
+            sb.append(String.valueOf(in));
+            sb.append(", ");
+        }
+
+        sb.append("_out=");
+        sb.append(String.valueOf(_out));
+        sb.append("]");
+
+        return sb.toString();
+    }
+}
